50d0df12c7c59da67ec7c884e1991687ecd6fe5e
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-03-21 12:18:35 -0400

    UPDATED site framework

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import Navbar from "./components/Navbar.js";
+import Hero from "./components/Hero.js";
+import About from "./components/About.js";
+import Projects from "./components/Projects.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
-    <div>Hello</div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Footer />
+    </>
   );
 }
 
