1e800c61767f74cc09ed5105e6f0afe8a809e3d2
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-03-23 12:39:41 -0400

    UPDATED portfolio section, styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,31 +1,97 @@
 export default function Projects() {
     return (
-        <div name="projects" className="w-full md:h-screen text-white">
+        <div name="projects" className="w-full md:h-screen text-white my-[12rem]">
             <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-black">Projects</p>
                     <p className="py-6">Check out some of my past projects</p>
                 </div>
-                <div className="grid sm:grid-col-2 md:grid-col-3 gap-4">
-                    <div className="shadow-lg shadow-[#000000] group container rounded-md flex justify-center items-center mx-auto content-div">
-                        <div>
-                            <span>
 
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    <div className="shadow-lg shadow-[#000000] group container rounded-md flex justify-center items-center mx-auto content-div"
+                    style={{ backgroundImage: "url('https://portfolio-site-assets-jg.s3.amazonaws.com/cert-gamer-preview.webp')" }}>
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
                             </span>
-                            <div>
+                            <div className="pt-8 text-center">
                                 <a href="/">
-                                    <button>
-
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Demo
                                     </button>
                                 </a>
                                 <a href="/">
-                                    <button>
-
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Code
                                     </button>
                                 </a>
                             </div>
                         </div>
                     </div>
+
+                    <div className="shadow-lg shadow-[#000000] group container rounded-md flex justify-center items-center mx-auto content-div"
+                    style={{ backgroundImage: "url('https://portfolio-site-assets-jg.s3.amazonaws.com/cert-gamer-preview.webp')" }}>
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Demo
+                                    </button>
+                                </a>
+                                <a href="/">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Code
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="shadow-lg shadow-[#000000] group container rounded-md flex justify-center items-center mx-auto content-div"
+                    style={{ backgroundImage: "url('https://portfolio-site-assets-jg.s3.amazonaws.com/cert-gamer-preview.webp')" }}>
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Demo
+                                    </button>
+                                </a>
+                                <a href="/">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Code
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="shadow-lg shadow-[#000000] group container rounded-md flex justify-center items-center mx-auto content-div"
+                    style={{ backgroundImage: "url('https://portfolio-site-assets-jg.s3.amazonaws.com/cert-gamer-preview.webp')" }}>
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Demo
+                                    </button>
+                                </a>
+                                <a href="/">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-white font-bold text-lg hover:scale-110 hover:shadow-lg hover:ring-2 hover:ring-white hover:ring-opacity-100">
+                                        Code
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                
                 </div>
             </div>
         </div>
